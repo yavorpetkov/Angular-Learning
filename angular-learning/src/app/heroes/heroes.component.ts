e812c89bcb7078861ab85e2bfe0c784ace97a283
Yavor Petkov <hermsues@gmail.com>
2022-01-20 10:27:44 +0200

    hero-detail

--- angular-learning/src/app/heroes/heroes.component.ts
+++ angular-learning/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 
 @Component({
@@ -11,6 +12,11 @@ export class HeroesComponent implements OnInit {
 		id: 1,
 		name: 'Windstorm'
 	};
+	selectedHero?: Hero;
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
+	heroes = HEROES;
 
 	constructor() {}
 
