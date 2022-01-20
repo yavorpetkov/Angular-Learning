623f0fee8f3150e4443257d536cd116103d5bb12
Yavor Petkov <hermsues@gmail.com>
2022-01-20 15:04:18 +0200

    services

--- angular-learning/src/app/app.module.ts
+++ angular-learning/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-	declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+	declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent ],
 	imports: [ BrowserModule, BrowserModule, FormsModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
--- angular-learning/src/app/heroes/heroes.component.ts
+++ angular-learning/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
 	selector: 'app-heroes',
@@ -8,17 +10,22 @@ import { Hero } from '../hero';
 	styleUrls: [ './heroes.component.css' ]
 })
 export class HeroesComponent implements OnInit {
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
-	};
+	constructor(private heroService: HeroService, private messageService: MessageService) {}
+
 	selectedHero?: Hero;
+
+	heroes: Hero[] = [];
+
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
+		this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
 	}
-	heroes = HEROES;
 
-	constructor() {}
+	getHeroes(): void {
+		this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+	}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.getHeroes();
+	}
 }
