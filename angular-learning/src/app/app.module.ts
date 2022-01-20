ff1d1edefd98fe45caa636f771e607a4bde78794
Yavor Petkov <hermsues@gmail.com>
2022-01-20 10:27:30 +0200

    mock-heroes

--- angular-learning/src/app/app.module.ts
+++ angular-learning/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 
 @NgModule({
-	declarations: [ AppComponent, HeroesComponent ],
+	declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
 	imports: [ BrowserModule, BrowserModule, FormsModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
