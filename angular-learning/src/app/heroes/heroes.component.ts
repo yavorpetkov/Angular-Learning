8a3a405c936e54e2fde3b1c5c92edbc4f031464f
Yavor Petkov <hermsues@gmail.com>
2022-01-24 14:25:56 +0200

    routing and dashboard

--- angular-learning/src/app/heroes/heroes.component.ts
+++ angular-learning/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -10,22 +11,15 @@ import { MessageService } from '../message.service';
 	styleUrls: [ './heroes.component.css' ]
 })
 export class HeroesComponent implements OnInit {
-	constructor(private heroService: HeroService, private messageService: MessageService) {}
-
-	selectedHero?: Hero;
-
 	heroes: Hero[] = [];
 
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-		this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+	constructor(private heroService: HeroService) {}
+
+	ngOnInit(): void {
+		this.getHeroes();
 	}
 
 	getHeroes(): void {
 		this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-	}
-
-	ngOnInit(): void {
-		this.getHeroes();
 	}
 }
