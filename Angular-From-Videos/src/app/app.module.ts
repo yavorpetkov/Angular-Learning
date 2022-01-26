8a321a6117f9d156b5750388f648e9909c5ffa1d
Yavor Petkov <hermsues@gmail.com>
2022-01-26 14:08:17 +0200

    tour of heroes tutorial

---
+++ Angular-From-Videos/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemsComponent } from './items/items.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- angular-learning/src/app/hero-detail/hero-detail.component.ts
+++ angular-learning/src/app/hero-detail/hero-detail.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -11,19 +11,26 @@ import { HeroService } from '../hero.service';
 	styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-	@Input() hero?: Hero;
+	hero: Hero | undefined;
+
 	constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) {}
 
 	ngOnInit(): void {
 		this.getHero();
 	}
 
+	getHero(): void {
+		const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+		this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+	}
+
 	goBack(): void {
 		this.location.back();
 	}
 
-	getHero(): void {
-		const id = Number(this.route.snapshot.paramMap.get('id'));
-		this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+	save(): void {
+		if (this.hero) {
+			this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+		}
 	}
 }
